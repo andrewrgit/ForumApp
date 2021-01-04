495b24c502c13663d8da6daaa6e345aa21f75ff6
andrewrgit <42653987+andrewrgit@users.noreply.github.com>
2021-01-04 00:20:48 -0600

    add jwt library, refactored login and account creation methods, implemented jwt

--- app.js
+++ app.js
@@ -2,88 +2,192 @@ const express = require("express");
 const path = require("path");
 const { Client } = require("pg");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 
-//Setup port
-var port = process.env.PORT;
-if(!port){
-    port = 3000;
+
+class APIResponse{
+    constructor(success, message){
+        this.success = success;
+        this.message = message;
+    }
 }
 
-//Setup db conn string
-var connString = process.env.DATABASE_URL;
-if(!connString){
-    connString = "postgres://postgres:password@localhost:5432/forumDb";
-}
+
+//Setup env variables
+const port = process.env.PORT || 3000;
+const connString = process.env.DATABASE_URL || "postgres://postgres:password@localhost:5432/forumDb";
+const privateKey = process.env.PRIVATE_KEY || "testprivatekey";
+
+
 
 app.use(express.static(path.resolve(__dirname, "public")));
 app.use(express.urlencoded( { extended: true }));
 app.use(express.json());
+
+function checkTokens(req, res, next){
+    const authHeader = req.headers['authorization'];
+    console.log(authHeader);
+    const token = authHeader && authHeader.split(' ')[1];
+    console.log(token);
+    next();
+}
+
+app.use(checkTokens);
 
 app.listen(port, () => {
     console.log(`Listening on http://localhost:${port}`);
 })
 
 app.post("/api/login", (req, res) => {
-    if(req.body.username == "phil"){
-        console.log("name is phil");
-        res.status(200).send("nice phil");
+
+    const {username, password} = req.body;
+    if(username && password){
+        loginAccount(username, password)
+        .then(apiResponse => {
+            if(apiResponse.success){               
+                const token = jwt.sign({ username }, privateKey, {
+                    expiresIn: 20
+                });
+                console.log("jwt token: " + token);
+                res.cookie("token", token, {
+                    maxAge: 22 * 1000
+                })
+                res.status(200).send("logged in successfully, assigning jwt");
+            }
+            else{
+                res.status(401).send(apiResponse.message);
+            }
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(400).send(err);
+        })
     }
-    else
-    console.log("Youre not phil")
+    else{
+        res.status(401).send("invalid login");
+    }
 })
 
 app.post("/api/createaccount", (req, res) => {
     if(req.body.username && req.body.password){
-        try{
-            if(createAccount(req.body.username, req.body.password)){
+        createAccount(req.body.username, req.body.password)
+        .then(apiResponse => {
+            if(apiResponse.success){
+                console.log("sending OK status");
                 res.sendStatus(200);
             }
             else{
                 res.status(400).send({
-                    message: "Account already exists"
+                    message: apiResponse.message
                 })
             }
-        }
-        catch (err){
+        })
+        .catch(err => {
+            console.log(err);
             res.status(400).send({
                 message: err
             });
-        }
+        })
+    }
+    else{
+        res.status(400).send("Username and password required when creating account");
     }
 })
 
 
 
-function createAccount(username, password){
+async function loginAccount(username, password){
+    if(await doesUsernameExist(username)){
+        let client = new Client({
+            connectionString: connString,
+            ssl: false
+        });
+
+        try {
+            await client.connect();
+
+            let queryString = "SELECT password FROM accounts WHERE username = $1;";
+            let values = [username];
+            const result = await client.query(queryString, values);
+
+            const isPasswordMatching = await bcrypt.compare(password, result.rows[0].password);
+            client.end();
+            if(isPasswordMatching){
+                return new APIResponse(true);
+            }
+        }
+        catch(err) {
+            console.log(err);
+            throw err;
+        }
+    }
+    else{
+        return new APIResponse(false, "Username does not exist");
+    }
+}
+
+async function createAccount(username, password){
+
+    if(await doesUsernameExist(username)){
+        return new APIResponse(false, "Username already exists");
+    }
+    else{
+        let client = new Client({
+            connectionString: connString,
+            ssl: false
+        });
+
+        const wasCreated = await client.connect()
+        .then(() => {
+            return bcrypt.hash(password, 12)
+        })
+        .then( hash => {
+            let queryString = "INSERT INTO accounts (username, password) VALUES ($1, $2);";
+            let values = [username, hash];
+            
+            return client.query(queryString, values);
+        })
+        .then(() => {
+            return new APIResponse(true);
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        })
+        client.end();
+        return wasCreated;
+    }
+}
+
+async function doesUsernameExist(username){
     let client = new Client({
         connectionString: connString,
         ssl: false
     });
 
-    client.connect();
-
     let checkUsernameQueryString = "SELECT username FROM accounts WHERE username = $1;";
     let values = [username]
 
-    //Check if username already exists
-    let doesAccountExist = false;
-    client.query(checkUsernameQueryString, values, (err, res) => {
-        if(err) throw err;
-        if(res.rowCount > 0){
-            doesAccountExist = true;
+    
+    const exists = await client.connect()
+    .then(() => {
+        return client.query(checkUsernameQueryString, values);
+    })
+    .then(res => {
+        if(res.rows.length == 0){
+            return false;
+        }
+        else{
+            return true;
         }
     })
-    if(doesAccountExist) return false;
-
-    //If not, hash password and create account
-    bcrypt.hash(password, 10).then( hash => {
-        let queryString = "INSERT INTO accounts (username, password) VALUES ($1, $2);";
-        let values = [username, password];
-
-        client.query(queryString, values, (err, res) => {
-            if(err) throw err;
-        })
+    .catch(err => {
+        console.log(err);
+        throw err;
     })
+    client.end();
+    return exists;
 }
+
